a2d7504043f25fd7556e994047fbb1ac1be7422f
bnnzz <ben31_rc@hotmail.com>
2025-02-18 22:35:33 -0600

    se modifico practica 4 validandola

--- practicas/p04/XHTML.php
+++ practicas/p04/XHTML.php
@@ -146,6 +146,10 @@ echo "b. Sistema operativo del servidor: " . PHP_OS . "<br />";  // También se 
 // c. Idioma del navegador del cliente
 echo "c. Idioma del navegador (cliente): " . $_SERVER['HTTP_ACCEPT_LANGUAGE'] . "<br />";
 ?>
+<p>
+    <a href="https://validator.w3.org/markup/check?uri=referer"><img
+      src="https://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" /></a>
+  </p>
 </body>
 </html>
 
