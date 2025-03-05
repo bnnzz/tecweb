2af7005f51f2dde25145335170ba420eb8f25bc2
bnnzz <ben31_rc@hotmail.com>
2025-03-04 18:25:58 -0600

    se agrego la busqueda de nombre en read.php

--- practicas/p10/p10-base/product_app/backend/read.php
+++ practicas/p10/p10-base/product_app/backend/read.php
@@ -21,8 +21,32 @@
 		} else {
             die('Query Error: '.mysqli_error($conexion));
         }
-		$conexion->close();
     } 
+// SI NO SE RECIBE UN ID, SE VERIFICA SI SE RECIBIÓ UN TÉRMINO DE BÚSQUEDA
+elseif (isset($_POST['search'])) {
+    $search = $_POST['search'];
+
+    // SE REALIZA LA QUERY DE BÚSQUEDA FLEXIBLE
+    if ($result = $conexion->query("SELECT * FROM productos WHERE nombre LIKE '%{$search}%' OR marca LIKE '%{$search}%' OR detalles LIKE '%{$search}%'")) {
+        $rows = $result->fetch_all(MYSQLI_ASSOC);
+
+        if (!empty($rows)) {
+            foreach ($rows as $row) {
+                $producto = array();
+                foreach ($row as $key => $value) {
+                    $producto[$key] = $value; // utf8_encode($value);
+                }
+                $data[] = $producto;
+            }
+        }
+        $result->free();
+    } else {
+        die('Query Error: '.mysqli_error($conexion));
+    }
+}
+
+$conexion->close();
+
     
     // SE HACE LA CONVERSIÓN DE ARRAY A JSON
     echo json_encode($data, JSON_PRETTY_PRINT);
