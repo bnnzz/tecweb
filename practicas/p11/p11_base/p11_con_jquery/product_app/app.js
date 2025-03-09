e3eae565a7bb278808e13653cb9bb569af915943
bnnzz <ben31_rc@hotmail.com>
2025-03-08 21:10:19 -0600

    se hacen modificaciones al app.js

---
+++ practicas/p11/p11_base/p11_con_jquery/product_app/app.js
@@ -0,0 +1,70 @@
+
+$(document).ready(function() {
+    console.log('JQuery funciona');
+
+    
+    // JSON BASE A MOSTRAR EN FORMULARIO
+    var baseJSON = {
+        "precio": 0.0,
+        "unidades": 1,
+        "modelo": "XX-000",
+        "marca": "NA",
+        "detalles": "NA",
+        "imagen": "img/default.png"
+      };
+    
+    function init() {
+        /**
+         * Convierte el JSON a string para poder mostrarlo
+         * ver: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/JSON
+         */
+        var JsonString = JSON.stringify(baseJSON,null,2);
+        document.getElementById("description").value = JsonString;
+    
+        // SE LISTAN TODOS LOS PRODUCTOS
+        //listarProductos();
+    }
+    
+
+
+
+$('#search').keyup(function(e) {
+    let search = $('#search').val();
+
+    $.ajax({
+        url: 'backend/product-search.php',
+        type: 'POST',
+        data: {search: search},
+        success:function(response) {
+            console.log(response);
+        }
+    
+
+
+
+    })
+
+
+
+
+
+ 
+    })
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
