9a04b051f7eb5a34ffd43301bafd78a998ba7c1f
bnnzz <ben31_rc@hotmail.com>
2025-03-09 12:57:48 -0600

    se agrego el mostar los productos al entrar a la pagina, y agregar productos

--- practicas/p11/p11_base/p11_con_jquery/product_app/app.js
+++ practicas/p11/p11_base/p11_con_jquery/product_app/app.js
@@ -50,13 +50,8 @@ $(document).ready(function() {
 
                 $('#container').html(template);
                 console.log('Contenido del contenedor actualizado');
+                $('#product-result').show();
 
-                // Mostrar el contenedor de resultados si hay productos
-                if (products.length > 0) {
-                    $('#product-result').removeClass('d-none');
-                } else {
-                    $('#product-result').addClass('d-none');
-                }
             },
         
         });
@@ -67,6 +62,81 @@ $(document).ready(function() {
 
 
     });
+
+
+
+    $('#product-form').submit(function(e) {
+        e.preventDefault();
+
+        // SE OBTIENE DESDE EL FORMULARIO EL JSON A ENVIAR
+        var productoJsonString = $('#description').val();
+        // SE CONVIERTE EL JSON DE STRING A OBJETO
+        var finalJSON = JSON.parse(productoJsonString);
+        // SE AGREGA AL JSON EL NOMBRE DEL PRODUCTO
+        finalJSON['nombre'] = $('#name').val();
+        // SE OBTIENE EL STRING DEL JSON FINAL
+        productoJsonString = JSON.stringify(finalJSON, null, 2);
+
+        $.ajax({
+            url: 'backend/product-add.php',
+            type: 'POST',
+            contentType: 'application/json',
+            data: productoJsonString,
+            success: function(response) {
+                console.log('Datos enviados:', response);
+                let respuesta = JSON.parse(response);
+                let template_bar = '';
+                template_bar += `
+                    <li style="list-style: none;">status: ${respuesta.status}</li>
+                    <li style="list-style: none;">message: ${respuesta.message}</li>
+                `;
+
+                // SE HACE VISIBLE LA BARRA DE ESTADO
+                $('#product-result').removeClass('d-none').addClass('d-block');
+                // SE INSERTA LA PLANTILLA PARA LA BARRA DE ESTADO
+                $('#container').html(template_bar);
+
+                // SE LISTAN TODOS LOS PRODUCTOS
+                //listarProductos();
+            }
+        });
+    });
+
+
+    $.ajax({
+        url: 'backend/product-list.php',
+        type: 'GET',
+        success: function(response) {
+            let products = JSON.parse(response);
+            let template = '';
+            products.forEach(product => {
+                let description = `
+                    <ul>
+                        <li>precio: ${product.precio}</li>
+                        <li>unidades: ${product.unidades}</li>
+                        <li>modelo: ${product.modelo}</li>
+                        <li>marca: ${product.marca}</li>
+                        <li>detalles: ${product.detalles}</li>
+                    </ul>
+                `;
+                template += `
+                    <tr>
+                        <td>${product.id}</td>
+                        <td>${product.nombre}</td>
+                        <td>${description}</td>
+                    </tr>
+                `;
+            });
+            $('#products').html(template);
+        },
+    });
+
+
+
+
+
+
+
 
     // Inicializar la función init
     init();
