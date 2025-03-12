08d84845ee00851b6754f78c764d93e0c56761e0
bnnzz <ben31_rc@hotmail.com>
2025-03-12 13:24:30 -0600

    se agrega carpetas

---
+++ ejemplos/php/POO/01-definicion/persona.php
@@ -0,0 +1,27 @@
+<?php   
+
+class Persona{
+
+    private $nombre;
+
+
+
+
+    public function inicializar($nombre){
+        $this->nombre=$nombre;
+
+    }
+
+
+    public function mostrar(){
+        echo '<p>'. $this->nombre .'</p>';
+       
+    }
+
+
+}
+
+
+
+
+?>
