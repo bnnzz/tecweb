4d67e9ecb13912f84259ab2a00a0bf556f6dee89
bnnzz <ben31_rc@hotmail.com>
2025-04-10 23:50:09 -0600

    avnaces

---
+++ practicas/p17/index.php
@@ -0,0 +1,37 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+require './vendor/autoload.php';
+$app = AppFactory::create();
+$app->setBasepath("/tecweb/practicas/p17");
+$app->get('/',function(Request $request, Response $response, $args) {
+    $response->getBody()->write("hola mundo slim");
+    return $response;
+});
+
+
+$app->get("/hola[/{nombre}]",function(Request $request, Response $response, $args){
+    $response->getBody()->write("Hola,". $args["nombre"]);
+    return $response;
+});
+
+$app->post("/pruebapost",function(Request $request, Response $response, $args){
+    $reqPost=$request->getParsedBody();
+    $val1=$reqPost["val1"];
+    $val2=$reqPost["val2"];
+    $response->getBody()->write("Valores: ". $val1 . " " . $val2);
+    return $response;
+});
+
+$app->get("/testjson",function(Request $request, Response $response, $args){
+    $reqPost=$request->getQueryParams();
+
+    $data[0]["nombre"]=$reqPost["nombre"];
+    $data[0]["apellidos"]=$reqPost["apellidos"];
+
+    $response->getBody()->write(json_encode($data,JSON_PRETTY_PRINT));
+    return $response;
+});
+$app->run();
+?>
