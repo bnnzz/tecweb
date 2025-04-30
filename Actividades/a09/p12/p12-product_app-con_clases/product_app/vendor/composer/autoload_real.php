0cb0e648c36174a402ca2976132beb517bcd8ae4
bnnzz <ben31_rc@hotmail.com>
2025-04-30 14:05:30 -0600

    se agrega slim y htdaccess

--- Actividades/a09/p12/p12-product_app-con_clases/product_app/vendor/composer/autoload_real.php
+++ Actividades/a09/p12/p12-product_app-con_clases/product_app/vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInited9fe29148d4274bd131b956e45f95c9
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInited9fe29148d4274bd131b956e45f95c9', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInited9fe29148d4274bd131b956e45f95c9', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInited9fe29148d4274bd131b956e45f95c9
         call_user_func(\Composer\Autoload\ComposerStaticInited9fe29148d4274bd131b956e45f95c9::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInited9fe29148d4274bd131b956e45f95c9::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
